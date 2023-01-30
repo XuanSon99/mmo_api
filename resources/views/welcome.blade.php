62a760dc926cde97ef4b41a5af2c33cd68e35019
XuanSon99 <nguyensonn124@gmail.com>
2023-01-30 22:29:43 +0700

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,11 @@
     <link rel="icon" href="/img/logo.png">
     <title>Chợ OTC Việt Nam</title>
     <link href="/css/chunk-a54b3e78.ee068850.css" rel="prefetch">
-    <link href="/js/chunk-1e2a9846.90703bcd.js" rel="prefetch">
+    <link href="/js/chunk-1e2a9846.96af8110.js" rel="prefetch">
     <link href="/js/chunk-a54b3e78.3155fe4a.js" rel="prefetch">
     <link href="/css/app.a8531d86.css" rel="preload" as="style">
     <link href="/css/chunk-vendors.adf0cae2.css" rel="preload" as="style">
-    <link href="/js/app.14dcaaf3.js" rel="preload" as="script">
+    <link href="/js/app.6e20e2bf.js" rel="preload" as="script">
     <link href="/js/chunk-vendors.fc952368.js" rel="preload" as="script">
     <link href="/css/chunk-vendors.adf0cae2.css" rel="stylesheet">
     <link href="/css/app.a8531d86.css" rel="stylesheet">
@@ -24,7 +24,7 @@
 <body>
     <div id="app"></div>
     <script src="/js/chunk-vendors.fc952368.js"></script>
-    <script src="/js/app.14dcaaf3.js"></script>
+    <script src="/js/app.6e20e2bf.js"></script>
 </body>
 
 </html>
