aa75f1f35997ad87b6ca1ebeca24898123cf9800
XuanSon99 <nguyensonn124@gmail.com>
2023-02-10 18:10:07 +0700

    cahnge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,21 +10,24 @@
     <meta property="og:url" content="">
     <link rel="icon" href="/img/logo.png">
     <title>Chợ OTC Việt Nam</title>
-    <link href="/css/chunk-a54b3e78.ee068850.css" rel="prefetch">
-    <link href="/js/chunk-1e2a9846.96af8110.js" rel="prefetch">
-    <link href="/js/chunk-a54b3e78.3155fe4a.js" rel="prefetch">
-    <link href="/css/app.a8531d86.css" rel="preload" as="style">
+    <link href="/css/chunk-37d18120.ee068850.css" rel="prefetch">
+    <link href="/js/chunk-05900bc2.061d9860.js" rel="prefetch">
+    <link href="/js/chunk-2d217a99.f6378490.js" rel="prefetch">
+    <link href="/js/chunk-304b9ae3.4062764a.js" rel="prefetch">
+    <link href="/js/chunk-37d18120.8ed681d7.js" rel="prefetch">
+    <link href="/js/chunk-6a8e5fc1.279221a0.js" rel="prefetch">
+    <link href="/css/app.7b8ed208.css" rel="preload" as="style">
     <link href="/css/chunk-vendors.adf0cae2.css" rel="preload" as="style">
-    <link href="/js/app.6e20e2bf.js" rel="preload" as="script">
-    <link href="/js/chunk-vendors.fc952368.js" rel="preload" as="script">
+    <link href="/js/app.11a10180.js" rel="preload" as="script">
+    <link href="/js/chunk-vendors.1d187da9.js" rel="preload" as="script">
     <link href="/css/chunk-vendors.adf0cae2.css" rel="stylesheet">
-    <link href="/css/app.a8531d86.css" rel="stylesheet">
+    <link href="/css/app.7b8ed208.css" rel="stylesheet">
 </head>
 
 <body>
     <div id="app"></div>
-    <script src="/js/chunk-vendors.fc952368.js"></script>
-    <script src="/js/app.6e20e2bf.js"></script>
+    <script src="/js/chunk-vendors.1d187da9.js"></script>
+    <script src="/js/app.11a10180.js"></script>
 </body>
 
 </html>
