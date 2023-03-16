9131036b2cbdd0f34d3820bd484cd37b6e01a4f9
Your Name <you@example.com>
2023-03-16 16:26:23 +0700

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,24 +10,28 @@
     <meta property="og:url" content="">
     <link rel="icon" href="/img/logo.png">
     <title>Chợ OTC Việt Nam</title>
-    <link href="/css/chunk-59e64ef3.ee068850.css" rel="prefetch">
-    <link href="/js/chunk-0356bbc9.dd56c180.js" rel="prefetch">
-    <link href="/js/chunk-05900bc2.00029b62.js" rel="prefetch">
-    <link href="/js/chunk-2d217a99.814b44f4.js" rel="prefetch">
-    <link href="/js/chunk-304b9ae3.4062764a.js" rel="prefetch">
-    <link href="/js/chunk-59e64ef3.396b8ca7.js" rel="prefetch">
-    <link href="/css/app.76af4a86.css" rel="preload" as="style">
-    <link href="/css/chunk-vendors.adf0cae2.css" rel="preload" as="style">
-    <link href="/js/app.86dd5744.js" rel="preload" as="script">
-    <link href="/js/chunk-vendors.1d187da9.js" rel="preload" as="script">
-    <link href="/css/chunk-vendors.adf0cae2.css" rel="stylesheet">
-    <link href="/css/app.76af4a86.css" rel="stylesheet">
+    <link href="/css/chunk-426e7fd5.ee068850.css" rel="prefetch">
+    <link href="/js/chunk-05900bc2.d77297d4.js" rel="prefetch">
+    <link href="/js/chunk-0a0e0514.e40dc8a2.js" rel="prefetch">
+    <link href="/js/chunk-2da194b1.b2974941.js" rel="prefetch">
+    <link href="/js/chunk-3df3519c.55d25c1d.js" rel="prefetch">
+    <link href="/js/chunk-426e7fd5.cd3e6d5a.js" rel="prefetch">
+    <link href="/js/chunk-5cbfa1c4.61097876.js" rel="prefetch">
+    <link href="/js/chunk-73c343a5.f6dd265e.js" rel="prefetch">
+    <link href="/js/chunk-b3a816b0.e9d7561f.js" rel="prefetch">
+    <link href="/js/chunk-cdc4c50e.6ecc0d07.js" rel="prefetch">
+    <link href="/css/app.f92e88d1.css" rel="preload" as="style">
+    <link href="/css/chunk-vendors.162fdd32.css" rel="preload" as="style">
+    <link href="/js/app.b0feb1e5.js" rel="preload" as="script">
+    <link href="/js/chunk-vendors.d3d375fc.js" rel="preload" as="script">
+    <link href="/css/chunk-vendors.162fdd32.css" rel="stylesheet">
+    <link href="/css/app.f92e88d1.css" rel="stylesheet">
 </head>
 
 <body>
     <div id="app"></div>
-    <script src="/js/chunk-vendors.1d187da9.js"></script>
-    <script src="/js/app.86dd5744.js"></script>
+    <script src="/js/chunk-vendors.d3d375fc.js"></script>
+    <script src="/js/app.b0feb1e5.js"></script>
 </body>
 
 </html>
