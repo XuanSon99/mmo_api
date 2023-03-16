3eb50eca73b58b782c96cdbe7bf1b5edfc5c8d56
Your Name <you@example.com>
2023-03-16 16:29:23 +0700

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" href="/img/logo.png">
     <title>Chợ OTC Việt Nam</title>
     <link href="/css/chunk-426e7fd5.ee068850.css" rel="prefetch">
-    <link href="/js/chunk-05900bc2.d77297d4.js" rel="prefetch">
+    <link href="/js/chunk-05900bc2.131c66ee.js" rel="prefetch">
     <link href="/js/chunk-0a0e0514.e40dc8a2.js" rel="prefetch">
     <link href="/js/chunk-2da194b1.b2974941.js" rel="prefetch">
     <link href="/js/chunk-3df3519c.55d25c1d.js" rel="prefetch">
@@ -22,7 +22,7 @@
     <link href="/js/chunk-cdc4c50e.6ecc0d07.js" rel="prefetch">
     <link href="/css/app.f92e88d1.css" rel="preload" as="style">
     <link href="/css/chunk-vendors.162fdd32.css" rel="preload" as="style">
-    <link href="/js/app.b0feb1e5.js" rel="preload" as="script">
+    <link href="/js/app.38de45da.js" rel="preload" as="script">
     <link href="/js/chunk-vendors.d3d375fc.js" rel="preload" as="script">
     <link href="/css/chunk-vendors.162fdd32.css" rel="stylesheet">
     <link href="/css/app.f92e88d1.css" rel="stylesheet">
@@ -31,7 +31,7 @@
 <body>
     <div id="app"></div>
     <script src="/js/chunk-vendors.d3d375fc.js"></script>
-    <script src="/js/app.b0feb1e5.js"></script>
+    <script src="/js/app.38de45da.js"></script>
 </body>
 
 </html>
