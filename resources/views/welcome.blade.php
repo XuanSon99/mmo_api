18d6920990abb11e3cefc472c3ef4fa1f22c3bcb
Your Name <you@example.com>
2023-03-17 17:13:56 +0700

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,19 +19,19 @@
     <link href="/js/chunk-5cbfa1c4.61097876.js" rel="prefetch">
     <link href="/js/chunk-73c343a5.f6dd265e.js" rel="prefetch">
     <link href="/js/chunk-b3a816b0.e9d7561f.js" rel="prefetch">
-    <link href="/js/chunk-cdc4c50e.6ecc0d07.js" rel="prefetch">
-    <link href="/css/app.f92e88d1.css" rel="preload" as="style">
+    <link href="/js/chunk-cdc4c50e.b7331482.js" rel="prefetch">
+    <link href="/css/app.1fe58bbc.css" rel="preload" as="style">
     <link href="/css/chunk-vendors.162fdd32.css" rel="preload" as="style">
-    <link href="/js/app.38de45da.js" rel="preload" as="script">
+    <link href="/js/app.afba10cb.js" rel="preload" as="script">
     <link href="/js/chunk-vendors.d3d375fc.js" rel="preload" as="script">
     <link href="/css/chunk-vendors.162fdd32.css" rel="stylesheet">
-    <link href="/css/app.f92e88d1.css" rel="stylesheet">
+    <link href="/css/app.1fe58bbc.css" rel="stylesheet">
 </head>
 
 <body>
     <div id="app"></div>
     <script src="/js/chunk-vendors.d3d375fc.js"></script>
-    <script src="/js/app.38de45da.js"></script>
+    <script src="/js/app.afba10cb.js"></script>
 </body>
 
 </html>
