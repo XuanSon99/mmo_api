582ba4d5aa858f51096352117b6f4eecb145f0fa
XuanSon99 <nguyensonn124@gmail.com>
2023-04-20 00:23:39 +0700

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,49 @@
+<!DOCTYPE html>
+<html lang="">
 
-<!DOCTYPE html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><meta name="description" content="Website cập nhật tỷ giá & tin tức thị trường nhanh chóng, chính xác"><meta property="og:image" content="https://chootc.com/img/thumbnail.jpg"><meta property="og:url" content="https://chootc.com"><link rel="icon" href="/img/logo.png"><link rel="canonical" href="https://chootc.com"><title>Chợ OTC Việt Nam</title><script async src="https://www.googletagmanager.com/gtag/js?id=G-VZ17QS1KLX"></script><script>window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="Website cập nhật tỷ giá & tin tức thị trường nhanh chóng, chính xác">
+    <meta property="og:image" content="https://chootc.com/img/thumbnail.jpg">
+    <meta property="og:url" content="https://chootc.com">
+    <link rel="icon" href="/img/logo.png">
+    <link rel="canonical" href="https://chootc.com">
+    <title>Chợ OTC Việt Nam</title>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VZ17QS1KLX"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-    gtag('config', 'G-VZ17QS1KLX');</script><link href="/css/chunk-426e7fd5.ee068850.css" rel="prefetch"><link href="/js/chunk-05900bc2.8aa68dae.js" rel="prefetch"><link href="/js/chunk-0a0e0514.907eb14e.js" rel="prefetch"><link href="/js/chunk-2da194b1.edf1e539.js" rel="prefetch"><link href="/js/chunk-3df3519c.77904728.js" rel="prefetch"><link href="/js/chunk-426e7fd5.18edb6c5.js" rel="prefetch"><link href="/js/chunk-5cbfa1c4.34abf69c.js" rel="prefetch"><link href="/js/chunk-73c343a5.d2b5c7bd.js" rel="prefetch"><link href="/js/chunk-b3a816b0.e9d7561f.js" rel="prefetch"><link href="/js/chunk-cdc4c50e.55eb16e4.js" rel="prefetch"><link href="/css/app.fca29dec.css" rel="preload" as="style"><link href="/css/chunk-vendors.162fdd32.css" rel="preload" as="style"><link href="/js/app.d516a20e.js" rel="preload" as="script"><link href="/js/chunk-vendors.d3d375fc.js" rel="preload" as="script"><link href="/css/chunk-vendors.162fdd32.css" rel="stylesheet"><link href="/css/app.fca29dec.css" rel="stylesheet"></head><body><div id="app"></div><script src="/js/chunk-vendors.d3d375fc.js"></script><script src="/js/app.d516a20e.js"></script></body></html>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-VZ17QS1KLX');
+    </script>
+    <link href="/css/chunk-426e7fd5.ee068850.css" rel="prefetch">
+    <link href="/js/chunk-05900bc2.7ea5fa3b.js" rel="prefetch">
+    <link href="/js/chunk-0a0e0514.907eb14e.js" rel="prefetch">
+    <link href="/js/chunk-2da194b1.edf1e539.js" rel="prefetch">
+    <link href="/js/chunk-3df3519c.77904728.js" rel="prefetch">
+    <link href="/js/chunk-426e7fd5.02d50330.js" rel="prefetch">
+    <link href="/js/chunk-5cbfa1c4.34abf69c.js" rel="prefetch">
+    <link href="/js/chunk-73c343a5.d2b5c7bd.js" rel="prefetch">
+    <link href="/js/chunk-b3a816b0.e9d7561f.js" rel="prefetch">
+    <link href="/js/chunk-cdc4c50e.3877548a.js" rel="prefetch">
+    <link href="/css/app.a1666698.css" rel="preload" as="style">
+    <link href="/css/chunk-vendors.162fdd32.css" rel="preload" as="style">
+    <link href="/js/app.5f7d0b6b.js" rel="preload" as="script">
+    <link href="/js/chunk-vendors.d3d375fc.js" rel="preload" as="script">
+    <link href="/css/chunk-vendors.162fdd32.css" rel="stylesheet">
+    <link href="/css/app.a1666698.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app"></div>
+    <script src="/js/chunk-vendors.d3d375fc.js"></script>
+    <script src="/js/app.5f7d0b6b.js"></script>
+</body>
+
+</html>
