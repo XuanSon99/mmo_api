4a87699b4bb1d3a59e4868025b94baa65b9bb9de
Your Name <you@example.com>
2023-05-18 14:28:09 +0700

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 
-<!DOCTYPE html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><meta name="description" content="Website cập nhật tỷ giá & tin tức thị trường nhanh chóng, chính xác"><meta property="og:image" content="https://chootc.com/img/thumbnail.jpg"><meta property="og:url" content="https://chootc.com"><link rel="icon" href="/img/logo.png"><link rel="canonical" href="https://chootc.com"><title>Chợ OTC Việt Nam</title><script async src="https://www.googletagmanager.com/gtag/js?id=G-VZ17QS1KLX"></script><script>window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-
-    gtag('config', 'G-VZ17QS1KLX');</script><link href="/css/chunk-426e7fd5.ee068850.css" rel="prefetch"><link href="/js/chunk-05900bc2.298cbb38.js" rel="prefetch"><link href="/js/chunk-0a0e0514.5ee279d8.js" rel="prefetch"><link href="/js/chunk-2da194b1.379e4949.js" rel="prefetch"><link href="/js/chunk-3df3519c.c3d551c9.js" rel="prefetch"><link href="/js/chunk-426e7fd5.18edb6c5.js" rel="prefetch"><link href="/js/chunk-5cbfa1c4.7e813087.js" rel="prefetch"><link href="/js/chunk-73c343a5.2fd5d238.js" rel="prefetch"><link href="/js/chunk-b3a816b0.e9d7561f.js" rel="prefetch"><link href="/js/chunk-cdc4c50e.8f63c5d5.js" rel="prefetch"><link href="/css/app.5128c117.css" rel="preload" as="style"><link href="/css/chunk-vendors.162fdd32.css" rel="preload" as="style"><link href="/js/app.97ab0780.js" rel="preload" as="script"><link href="/js/chunk-vendors.d3d375fc.js" rel="preload" as="script"><link href="/css/chunk-vendors.162fdd32.css" rel="stylesheet"><link href="/css/app.5128c117.css" rel="stylesheet"></head><body><div id="app"></div><script src="/js/chunk-vendors.d3d375fc.js"></script><script src="/js/app.97ab0780.js"></script></body></html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    
+</body>
+</html>
