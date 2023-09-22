8add6afa88e313dc9e37a8228a63ed7d88bd0372
Your Name <you@example.com>
2023-09-22 11:35:32 +0700

    change

--- app/Http/Controllers/ProfitController.php
+++ app/Http/Controllers/ProfitController.php
@@ -13,13 +13,20 @@ class ProfitController extends Controller
 {
     public function index()
     {
-        return Profit::orderBy('created_at', 'DESC')->get();
+        return Profit::orderBy('date', 'DESC')->get();
     }
 
     public function store(Request $request)
     {
+        $profit = Profit::where('account', $request->account)->whereMonth('date', '=', date('m'))->first();
+
+        if($profit){
+            return response()->json(["status" => false, "message" => ["Tài khoản đã có lợi nhuận tháng " . date('m')]], 400);
+        }
+
         $data = new Profit([
             'account' => $request->account,
+            'balance' => $request->balance,
             'date' => $request->date,
             'profit' => $request->profit,
             'commission' => $request->commission
@@ -40,12 +47,18 @@ class ProfitController extends Controller
         return response()->json(["status" => true], 200);
     }
 
+    public function destroy(Profit $Profit)
+    {
+        $Profit->delete();
+        return response()->json(["status" => true], 200);
+    }
+
     public function updateBrokerageMoney()
     {
         $customers = Customer::orderBy('created_at', 'DESC')->get();
 
         foreach ($customers as $cus) {
-            $customer = Profit::where('account', $cus->account)->whereMonth('date', '=', date('m'))->first();
+            $customer = Profit::where('account', $cus->account)->where('balance', '>=', 500)->whereMonth('date', '=', date('m'))->first();
 
             if ($customer) {
                 $customer->update([
@@ -61,18 +74,23 @@ class ProfitController extends Controller
                 $money = ($customer->profit / 5 +  $customer->commission) / 2;
             }
 
-            $f1_profit = Profit::where('account', $cus->refferal)->whereMonth('date', '=', date('m'))->first();
+            $f1_profit = Profit::where('account', $cus->refferal)->where('balance', '>=', 500)->whereMonth('date', '=', date('m'))->first();
+            $f1_customer = Customer::where('account', $cus->refferal)->first();
 
             if ($f1_profit) {
+                $brokerage_money = $f1_profit->brokerage_money + $money * 7 / 10;
+
+                if($f1_customer->agency){
+                    $brokerage_money = $f1_profit->brokerage_money + $money;
+                }
+
                 $f1_profit->update([
-                    'brokerage_money' => $f1_profit->brokerage_money + $money * 7 / 10
+                    'brokerage_money' => $brokerage_money
                 ]);
             }
 
-            $f1_customer = Customer::where('account', $cus->refferal)->first();
-
             if ($f1_customer) {
-                $f0_profit = Profit::where('account', $f1_customer->refferal)->whereMonth('date', '=', date('m'))->first();
+                $f0_profit = Profit::where('account', $f1_customer->refferal)->where('balance', '>=', 500)->whereMonth('date', '=', date('m'))->first();
 
                 if ($f0_profit) {
                     $f0_profit->update([
