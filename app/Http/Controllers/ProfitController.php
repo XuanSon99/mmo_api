aa557fa510255381e3fe3110994c8c18e2353ea1
XuanSon99 <nguyensonn124@gmail.com>
2023-09-24 15:30:08 +0700

    change

--- app/Http/Controllers/ProfitController.php
+++ app/Http/Controllers/ProfitController.php
@@ -20,7 +20,7 @@ class ProfitController extends Controller
     {
         $profit = Profit::where('account', $request->account)->whereMonth('date', '=', date('m'))->first();
 
-        if($profit){
+        if ($profit) {
             return response()->json(["status" => false, "message" => ["Tài khoản đã có lợi nhuận tháng " . date('m')]], 400);
         }
 
@@ -70,8 +70,10 @@ class ProfitController extends Controller
         foreach ($customers as $cus) {
             $customer = Profit::where('account', $cus->account)->whereMonth('date', '=', date('m'))->first();
 
-            if($customer){
+            if ($customer) {
                 $money = ($customer->profit / 5 +  $customer->commission) / 2;
+            } else {
+                continue;
             }
 
             $f1_profit = Profit::where('account', $cus->refferal)->where('balance', '>=', 500)->whereMonth('date', '=', date('m'))->first();
@@ -80,7 +82,7 @@ class ProfitController extends Controller
             if ($f1_profit) {
                 $brokerage_money = $f1_profit->brokerage_money + $money * 7 / 10;
 
-                if($f1_customer->agency){
+                if ($f1_customer->agency) {
                     $brokerage_money = $f1_profit->brokerage_money + $money;
                 }
 
